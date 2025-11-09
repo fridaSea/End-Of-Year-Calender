49d3a570a94f5b17e1650c7e6ef9be32b7d9d18f
fridaSea <maria.kluge.codes@gmail.com>
2025-11-09 19:24:46 +0100

    Add contact page

---
+++ client/src/pages/Contact/Contact.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <div className="socialIcons">
+        <a
+          href="https://www.instagram.com/_frida_sea_"
+          target="_blank"
+          aria-label="Link to Instagram Account"
+        >
+          <span className="socialicon icon-instagram"></span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
