62d3da9cfd5517d2a2408c4c36ca4444e2828724
fridaSea <maria.kluge.codes@gmail.com>
2025-11-09 19:22:25 +0100

    Add calendar day page

---
+++ client/src/pages/Day/Day.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+function Day() {
+  const { dayId } = useParams();
+  return (
+    <div>
+      <h1>Tag {dayId}</h1>
+      <p>Hier kommt der Inhalt für Tag {dayId} hin.</p>
+    </div>
+  );
+}
+
+export default Day;
