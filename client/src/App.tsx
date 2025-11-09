b440c14146a6f932d07a4310a22e37380cea8fb9
fridaSea <maria.kluge.codes@gmail.com>
2025-11-09 19:25:55 +0100

    Add app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Outlet, Routes, Route } from "react-router";
+import "./App.css";
+import Calendar from "./pages/Calendar/Calendar";
+import Contact from "./pages/Contact/Contact";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Day from "./pages/Day/Day";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Root = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Root />}>
+            <Route index element={<Calendar />} />
+            <Route path="/day/:dayId" element={<Day />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
