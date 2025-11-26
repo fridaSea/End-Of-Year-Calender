f36702ebb21a3b3e77ba1ad035be0876b81ba526
fridaSea <maria.kluge.codes@gmail.com>
2025-11-26 21:46:20 +0100

    Add popup for closed days with Popover API

--- client/src/components/Popup/Popup.tsx
+++ client/src/components/Popup/Popup.tsx
@@ -1,20 +1,53 @@
+import { useEffect, useRef } from "react";
 import "./Popup.css";
 
 function Popup({ showPopup, closePopup, children }) {
-  if (!showPopup) {
-    return null;
-  }
-  return (
-    <div className="popup">
-      <div className="popup-inner">
-        <button className="close-btn" onClick={closePopup}>
-          <span className="icon icon-clear"></span>
-        </button>
+  const popoverRef = useRef(null);
 
-        {children}
+  useEffect(() => {
+    if (!popoverRef.current) return;
+
+    if (showPopup) {
+      popoverRef.current.showPopover();
+    } else {
+      popoverRef.current.hidePopover();
+    }
+  }, [showPopup]);
+
+  return (
+    <div id="mypopover" popover="manual" anchor="none" ref={popoverRef}>
+      <div className="popup-positioner">
+        <div className="popup-content">
+          <button className="close-btn" onClick={closePopup}>
+            <span className="icon icon-clear"></span>
+          </button>
+          {children}
+        </div>
       </div>
     </div>
   );
 }
 
 export default Popup;
+
+// function Popup({ showPopup, closePopup, children }) {
+//   if (!showPopup) {
+//     return null;
+//   }
+//   return (
+//     <div className="popup">
+//       <div popoverTarget="mypopover" className="popup-inner">
+//         <button className="close-btn" onClick={closePopup}>
+//           <span className="icon icon-clear"></span>
+//         </button>
+
+//         <div id="mypopover" popover="">
+//           {children}
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Popup;
